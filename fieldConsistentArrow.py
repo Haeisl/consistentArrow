b6901dd7b3a5434eb536f0094678b8b8814d623f
David Hasse <david_h_hasse@yahoo.de>
2024-05-10 23:08:13 +0200

    interpolation

--- fieldConsistentArrow.py
+++ fieldConsistentArrow.py
@@ -22,17 +22,13 @@ class consistentArrow(VTKPythonAlgorithmBase):
         self._length = 7
         self._steps = 2
         self._grid_dims = [1, 1]
-        '''
-        self._granularity = 0.05
-        self._tipLen = 5
-        self._ratioL = 0.5
-        self._ratioR = 0.5
-        '''
         VTKPythonAlgorithmBase.__init__(self, nInputPorts=1, nOutputPorts=1)
+
 
     @smproperty.stringvector(name="StringInfo", information_only="1")
     def GetStrings(self):
         return ["euler", "rk4"]
+
 
     @smproperty.stringvector(name="Variant", number_of_elements="1", default="rk4")
     @smdomain.xml(\
@@ -45,12 +41,14 @@ class consistentArrow(VTKPythonAlgorithmBase):
     def SetString(self, value):
         self._mode = value
         self.Modified()
-        
+
+
     @smproperty.doublevector(name="Center Point", default_values=[1., 1.])
     @smdomain.doublerange()
     def SetStartPoint(self, x, y):
         self._center = [x, y]
         self.Modified()
+
 
     @smproperty.intvector(name="Glyph Width", number_of_elements=1, default_values=1)
     @smdomain.intrange(min=0, max=5)
@@ -58,11 +56,13 @@ class consistentArrow(VTKPythonAlgorithmBase):
         self._thickness = d
         self.Modified()
 
+
     @smproperty.intvector(name="Glyph Length", number_of_elements=1, default_values=7)
     @smdomain.intrange(min=1, max=10)
     def SetLength(self, l):
         self._length = l
         self.Modified()
+
 
     @smproperty.intvector(name="Grid (rows | cols)", number_of_elements=2, default_values=[1,1])
     @smdomain.intrange(min=1,max=10)
@@ -70,15 +70,18 @@ class consistentArrow(VTKPythonAlgorithmBase):
         self._grid_dims = [rows, cols]
         self.Modified()
 
+
     @smproperty.intvector(name="Steps per One", number_of_elements=1, default_values=2)
     @smdomain.intrange(min=1, max=10)
     def SetGrain(self, d):
         self._steps = d
         self.Modified()
 
+
     def FillOutputPortInformation(self, port, info):
         info.Set(vtkDataObject.DATA_TYPE_NAME(), 'vtkPolyData')
         return 1
+
 
     def generate_grid_points(self, bounds):
         """
@@ -86,11 +89,51 @@ class consistentArrow(VTKPythonAlgorithmBase):
         """
         min_x, max_x, min_y, max_y = bounds[0],bounds[1],bounds[2],bounds[3]
         rows, cols = int(self._grid_dims[0]), int(self._grid_dims[1])
-        
+
         x = np.linspace(min_x + (max_x - min_x) / (cols + 1), max_x - (max_x - min_x) / (cols + 1), cols)
         y = np.linspace(min_y + (max_y - min_y) / (rows + 1), max_y - (max_y - min_y) / (rows + 1), rows)
-        
+
         xv, yv = np.meshgrid(x,y)
-        
+
         grid_points = np.column_stack([xv.ravel(), yv.ravel(), np.zeros(rows * cols)])
         return grid_points
+
+
+    def bilinear_interpolation(self, image_data, position):
+        """
+        Bilinear interpolation for obtaining the vector at a given position in the vector field.
+
+        Parameters:
+            image_data: vtk.vtkImageData
+                vtkImageData object containing the vector field data.
+            position: numpy.ndarray
+                Position at which to interpolate the vector. Shape: (2,)
+                The position should be in the image data coordinate system
+
+        Returns:
+            numpy.ndarray
+                Vector interpolated at the given position
+        """
+        # extract image dimensions
+        dims = image_data.GetDimensions()
+        i, j = position
+
+        # extract grid coordinates
+        i_floor, j_floor = int(np.floor(i)), int(np.floor(j))
+        i_ceil, j_ceil  = int(np.ceil(i)), int(np.ceil(j))
+
+        # bilinear interpolation weights
+        di = i - i_floor
+        dj = j - j_floor
+
+        # interpolate vectors
+        vector_field = image_data.GetPointData().GetArray(0)
+        vector_floor_floor = np.array(vector_field.GetTuple(np.ravel_multi_index((i_floor, j_floor, 0), dims)))
+        vector_floor_ceil = np.array(vector_field.GetTuple(np.ravel_multi_index((i_floor, j_ceil, 0), dims)))
+        vector_ceil_floor = np.array(vector_field.GetTuple(np.ravel_multi_index((i_ceil, j_floor, 0), dims)))
+        vector_ceil_ceil = np.array(vector_field.GetTuple(np.ravel_multi_index((i_ceil, j_ceil, 0), dims)))
+
+        interpolated_vector = (1 - di) * ((1 - dj) * vector_floor_floor + dj * vector_ceil_floor) + \
+                                di * ((1 - dj) * vector_floor_ceil + dj * vector_ceil_ceil)
+
+        return np.append(interpolated_vector, 0)
