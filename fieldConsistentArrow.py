273a986ed3334b3991270ed93cd24c1f4908c785
David <david_h_hasse@yahoo.de>
2024-05-10 23:00:25 +0200

    grid

--- fieldConsistentArrow.py
+++ fieldConsistentArrow.py
@@ -1,11 +1,13 @@
 from vtkmodules.util.vtkAlgorithm import VTKPythonAlgorithmBase
 from vtkmodules.vtkCommonDataModel import vtkImageData, vtkDataObject, vtkPolyData
+from vtk.vtkCommonCore import vtkDoubleArray
 from pyprtl.models.ModelBase import *
 import numpy as np
 import math
 import vtk
 import time
 #import multiprocessing
+
 
 from paraview.util.vtkAlgorithm import smproxy, smproperty, smdomain
 
@@ -19,16 +21,18 @@ class consistentArrow(VTKPythonAlgorithmBase):
         self._thickness = 1
         self._length = 7
         self._steps = 2
-        self._cols = 1
-        self._rows = 1
-
+        self._grid_dims = [1, 1]
+        '''
+        self._granularity = 0.05
+        self._tipLen = 5
+        self._ratioL = 0.5
+        self._ratioR = 0.5
+        '''
         VTKPythonAlgorithmBase.__init__(self, nInputPorts=1, nOutputPorts=1)
-
 
     @smproperty.stringvector(name="StringInfo", information_only="1")
     def GetStrings(self):
         return ["euler", "rk4"]
-
 
     @smproperty.stringvector(name="Variant", number_of_elements="1", default="rk4")
     @smdomain.xml(\
@@ -41,14 +45,12 @@ class consistentArrow(VTKPythonAlgorithmBase):
     def SetString(self, value):
         self._mode = value
         self.Modified()
-
-
+        
     @smproperty.doublevector(name="Center Point", default_values=[1., 1.])
     @smdomain.doublerange()
     def SetStartPoint(self, x, y):
         self._center = [x, y]
         self.Modified()
-
 
     @smproperty.intvector(name="Glyph Width", number_of_elements=1, default_values=1)
     @smdomain.intrange(min=0, max=5)
@@ -56,20 +58,17 @@ class consistentArrow(VTKPythonAlgorithmBase):
         self._thickness = d
         self.Modified()
 
-
     @smproperty.intvector(name="Glyph Length", number_of_elements=1, default_values=7)
     @smdomain.intrange(min=1, max=10)
     def SetLength(self, l):
         self._length = l
         self.Modified()
 
-
-    @smproperty.doublevector(name="Grid Rows | Cols", default_values=[0, 0])
-    @smdomain.intrange()
-    def SetGridDimensions(self, rows, cols):
-        self.grid_dimensions = [rows, cols]
+    @smproperty.intvector(name="Grid (rows | cols)", number_of_elements=2, default_values=[1,1])
+    @smdomain.intrange(min=1,max=10)
+    def SetGridDims(self, rows, cols):
+        self._grid_dims = [rows, cols]
         self.Modified()
-
 
     @smproperty.intvector(name="Steps per One", number_of_elements=1, default_values=2)
     @smdomain.intrange(min=1, max=10)
@@ -77,26 +76,21 @@ class consistentArrow(VTKPythonAlgorithmBase):
         self._steps = d
         self.Modified()
 
-
     def FillOutputPortInformation(self, port, info):
         info.Set(vtkDataObject.DATA_TYPE_NAME(), 'vtkPolyData')
         return 1
 
-
     def generate_grid_points(self, bounds):
         """
-        Create a grid of uniformly spread coordinates within the bounding box.
+        Create a grid of uniformly spaced coordinates within the bounding box.
         """
-        min_x, max_x, min_y, max_y = bounds[0], bounds[1], bounds[2], bounds[3]
-        columns, rows = self.grid_dimensions
-        # Generate the linearly spaced points between min and max for x and y
-        x = np.linspace(min_x + (max_x - min_x) / (columns + 1), max_x - (max_x - min_x) / (columns + 1), columns)
+        min_x, max_x, min_y, max_y = bounds[0],bounds[1],bounds[2],bounds[3]
+        rows, cols = int(self._grid_dims[0]), int(self._grid_dims[1])
+        
+        x = np.linspace(min_x + (max_x - min_x) / (cols + 1), max_x - (max_x - min_x) / (cols + 1), cols)
         y = np.linspace(min_y + (max_y - min_y) / (rows + 1), max_y - (max_y - min_y) / (rows + 1), rows)
-
-        # Generate grid points using meshgrid
-        xv, yv = np.meshgrid(x, y)
-
-        # Flatten the coordinates and stack them, then add a zero as the third component
-        grid_points = np.column_stack([xv.ravel(), yv.ravel(), np.zeros(rows * columns)])
-
+        
+        xv, yv = np.meshgrid(x,y)
+        
+        grid_points = np.column_stack([xv.ravel(), yv.ravel(), np.zeros(rows * cols)])
         return grid_points
